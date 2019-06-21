e4eaa326415fbe1e454dc8be337a53d55179daa9
Aaron Fulmer <33696641+AaronFulmer@users.noreply.github.com>
2019-06-21 19:47:09 -0400

    Add: Test script for PIR sensor

---
+++ pir_test.py
@@ -0,0 +1,9 @@
+from gpiozero import MotionSensor
+
+pin = 4                  # Define what GPIO pin the sensor will use -- I used pin 4
+pir = MotionSensor(pin)  # Create the sensor object using the pin that was defined
+
+while True:
+  pir.wait_for_motion()    # Tells the script to actively search for motion and pause until it finds it
+  print("Motion Detected") # Output a notice to ensure the sensor is working
+  pir.wait_for_no_motion() # Pause the script until motion is no longer detected
